d45477a637f1624071f8685bad391f4394e1e6b7
Courtney Radford <Courtney6554@Courtneys-MBP.attlocal.net>
2017-08-17 19:04:14 -0700

    psychic commit 3

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,65 +1,53 @@
     var wins = 1;
     var losses = 1;
-    var TOTAL_GUESSES = 10;
+    var guessesLeft = 10;
 
 
     var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
     var allUserGuess = [];
 
-    document.onkeydown = function(event){
+    document.onkeyup = function(event) {
         var userGuess = event.key;
 
         var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-
-        var guessesLeft = TOTAL_GUESSES--;
-
+        
         document.getElementById("allUserGuess").innerHTML = allUserGuess;
         document.getElementById("computerGuess").innerHTML = computerGuess;
         document.getElementById("guessesLeft").innerHTML = guessesLeft;
 
-
-        function myReset(x) {
-        	var x = guessesLeft + 10;
-        	document.getElementById("guessesLeft").innerHTML = x;
-        	return x;
-        	
-            document.getElementById("result").innerHTML = "Choose Any Letter";
-        
-
-        }
+        if (guessesLeft > 0) {
+        guessesLeft--;
+         } else if (guessesLeft == 0) {
+            guessesLeft = guessesLeft + 10;
+         }
 
         if (computerChoices.indexOf(userGuess) !== -1) {
             allUserGuess.push(" " + userGuess);
-            document.getElementById("result").innerHTML = "Not Valid Entry. Try Again."
+            document.getElementById("response").innerHTML = "Not Valid Entry. Try Again."
 
 
             if (userGuess === computerGuess) {
 
                 document.getElementById("wins").innerHTML = wins;
-                document.getElementById("result").innerHTML = "You Win! Press Any Key to Play Again!"
+                document.getElementById("response").innerHTML = "You Win! Press Any Key to Play Again!"
                 wins++;
+                guessesLeft = guessesLeft + (10 - guessesLeft);
 
 
             } else {
 
-                document.getElementById("result").innerHTML = "Try Again.";
+                document.getElementById("response").innerHTML = "Try Again.";
 
             }
 
 
             if ((guessesLeft === 0) && (userGuess !== computerGuess)) {
                 document.getElementById("losses").innerHTML = losses;
-                document.getElementById("result").innerHTML = "You Lose!  Press Any Key to Play Again";
+                document.getElementById("response").innerHTML = "You Lose!  Press Any Key to Play Again";
                 losses++;
 
             }
-            
-            if (guessesLeft < 0) {
-                myReset(10);
-
-            }
-
 
 
 
